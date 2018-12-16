7be60104fa81b42ead4ae11a31b3d7937d936a13
Mindysueasaurus <mymindyyeager@gmail.com>
2018-12-16 10:21:06 -0600

    working on a bug

--- data/server.js
+++ data/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const helmet = require('helmet')
+const helmet = require('helmet');
+const db = require('../data/helpers/notesHelpers');
 
 const noteRouter = require('../api/noteRouter');
 const server = express();
@@ -9,5 +10,15 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 server.use('/api/notes', noteRouter);
+
+server.get('/',  async (req, res) => {
+  try {
+      const notes = await db.getAll();
+      res.status(200).json(notes)
+  } 
+  catch (err) {
+      res.status(500).json(err);
+  }
+});
 
 module.exports = server;
