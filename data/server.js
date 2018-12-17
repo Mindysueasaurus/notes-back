26e41d3be73b149822fe0b0f10663fb111fb4933
Mindysueasaurus <mymindyyeager@gmail.com>
2018-12-17 10:48:23 -0600

    changes for heroku

--- data/server.js
+++ data/server.js
@@ -3,22 +3,26 @@ const cors = require('cors');
 const helmet = require('helmet');
 const db = require('../data/helpers/notesHelpers');
 
-//const noteRouter = require('../api/noteRouter');
+const noteRouter = require('../api/noteRouter');
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
-//server.use('/api/notes', noteRouter);
+server.use('/api/notes', noteRouter);
 
-server.get('/api/notes/',  async (req, res) => {
-  try {
-      const notes = await db.getAll();
-      res.status(200).json(notes)
-  } 
-  catch (err) {
-      res.status(500).json(err);
-  }
-});
+server.get('/', (req, res) => {
+    res.send(`Api running on port`)
+})
+
+// server.get('/api/notes/',  async (req, res) => {
+//   try {
+//       const notes = await db.getAll();
+//       res.status(200).json(notes)
+//   } 
+//   catch (err) {
+//       res.status(500).json(err);
+//   }
+// });
 
 module.exports = server;
